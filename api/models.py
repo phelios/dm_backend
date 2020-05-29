9ccf65e9689a55cfba05387875b9ee4f6abf70d2
phelios <phelio.xion@gmail.com>
2020-05-29 10:45:15 +1000

    Add models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Criterion(models.Model):
+    name = models.CharField(max_length=200)
+    weight = models.IntegerField()
+
+
+class CriteriaSet(models.Model):
+    name = models.CharField(max_length=200)
+    criterion = models.ForeignKey(Criterion, on_delete=models.CASCADE)
+
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    candidates = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    criteria_set = models.ForeignKey(CriteriaSet, on_delete=models.CASCADE)
+
+
+class CandidateScore(models.Model):
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    criterion = models.ForeignKey(Criterion, on_delete=models.CASCADE)
+    score = models.IntegerField()
--- dm_backend/settings.py
+++ dm_backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api.apps.ApiConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
